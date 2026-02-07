8e7e5b1873c57ba9bbfaab35dbf1596464e81d7c
Seng-Kimnam <sengkimnam97@gmail.com>
2026-02-07 22:13:56 +0700

    fix dashboard by role

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -1,18 +1,22 @@
 import { GetClient } from "@/Service/ClientService";
 import {
-  ArrowDownIcon,
+  // ArrowDownIcon,  
   ArrowUpIcon,
-  BoxIconLine,
+  // BoxIconLine,
   GroupIcon,
 } from "../../icons";
 import Badge from "../ui/badge/Badge";
 import { LucideTrendingUp } from "lucide-react";
 import { GetLawyers } from "@/Service/UserService";
 import { myAllRoles } from "@/api/role";
+import { GetTask } from "@/Service/TaskService";
+import { GetAllAppointment } from "@/Service/AppointmentService";
 
 export default function EcommerceMetrics() {
   const { clientList } = GetClient();
   const { list } = GetLawyers();
+  const { taskList, page } = GetTask();
+  const { appointmentList } = GetAllAppointment();
   const currentRole = localStorage.getItem("role");
 
   return (
@@ -72,8 +76,56 @@ export default function EcommerceMetrics() {
       {/* dashboard for lawyer role  */}
 
       {currentRole === myAllRoles[1].toLocaleUpperCase() && <>
-      Hello Lawyer
-      </>}
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+          {/* <!-- Metric Item Start --> */}
+          <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+            <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+              <LucideTrendingUp className="text-gray-800 size-6 dark:text-white/90" />
+            </div>
+
+            <div className="flex items-end justify-between mt-5">
+              <div>
+                <span className="text-lg dark:font-bold text-gray-500 dark:text-gray-400">
+                  Task
+                </span>
+                <h4 className="mt-2  font-bold text-3xl text-gray-800  dark:text-white/90">
+                  {taskList?.length}
+
+                  {taskList?.length > 1 ? " Requests" : " Request"}
+                </h4>
+              </div>
+              <Badge color="success">
+                <ArrowUpIcon />
+                {/* 11.01% */}
+              </Badge>
+            </div>
+          </div>
+          {/* <!-- Metric Item End --> */}
+
+          {/* <!-- Metric Item Start --> */}
+          <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+            <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+              <GroupIcon className="text-gray-800 size-6 dark:text-white/90" />
+            </div>
+            <div className="flex items-end justify-between mt-5">
+              <div>
+                <span className="text-lg text-gray-500 dark:text-gray-400">
+                  Appointment
+                </span>
+                <h4 className="mt-2 text-3xl   font-bold text-gray-800 dark:text-white/90">
+                  {appointmentList.length} {list.length > 1 ? "Lawyers" : "Lawyer"}
+                </h4>
+              </div>
+              <Badge color="success">
+                <ArrowUpIcon />
+                {/* 11.01% */}
+              </Badge>
+            </div>
+          </div>
+          {/* <!-- Metric Item End --> */}
+        </div>
+      </>
+      }
     </>
   );
 }
